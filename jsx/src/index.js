5d6ab53e5b0ce63adb486defda91ed148364a1e3
vishnupsatish <vishnupavan.satish@gmail.com>
2021-12-04 20:56:26 -0500

    First commit

---
+++ jsx/src/index.js
@@ -0,0 +1,14 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = () => {
+    return <div>Hi there!</div>;
+};
+
+// Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
